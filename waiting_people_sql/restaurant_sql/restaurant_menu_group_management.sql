8020fbc234337cdeb5ae49fac7e92e67cc95b3e2
leebackcoding <lcy2825@gmail.com>
2024-07-29 12:02:08 +0900

    convention revision

--- waiting_people_sql/restaurant_sql/restaurant_menu_group_management.sql
+++ waiting_people_sql/restaurant_sql/restaurant_menu_group_management.sql
@@ -1,58 +1,58 @@
 -- 메뉴 그룹 등록 by changyoun
 INSERT
   INTO tb_menu_group(
-  							restaurant_seq     
-						 , menu_group_name
-						 , menu_group_detail)
+  					  restaurant_seq     
+					, menu_group_name
+					, menu_group_detail)
 VALUES(
-		 1
-	 , '둘이 먹기 딱 좋은!'
-	 , '둘이 먹기 좋은 양으로 푸짐하게 제공합니다.');
+		1
+	, '둘이 먹기 딱 좋은!'
+	, '둘이 먹기 좋은 양으로 푸짐하게 제공합니다.');
 	 
 	 
 -- 메뉴 그룹 최조 조회 모습 (최초 등록 시간 기준 오래된 순)
 -- by changyoun
 SELECT
-		 menu_group_name
+	   menu_group_name
   FROM tb_menu_group
  ORDER BY FIELD(
- 					 menu_group_name
- 				  , '메인메뉴'
-				  , '애피타이저'
-				  , '디저트'
-				  , '기본메뉴');
+ 				 menu_group_name
+ 			   , '메인메뉴'
+			   , '애피타이저'
+			   , '디저트'
+			   , '기본메뉴');
   
 -- 메뉴 그룹 이름 순으로 조회 (오름차순)
 -- by changyoun
 SELECT
-		 menu_group_name
+	   menu_group_name
   FROM tb_menu_group
  ORDER BY FIELD(
- 					 menu_group_name
- 				  , '메인메뉴'
-				  , '애피타이저'
-				  , '디저트'
-				  , '기본메뉴'),
- 		 menu_group_name;
+ 				 menu_group_name
+ 			   , '메인메뉴'
+			   , '애피타이저'
+			   , '디저트'
+			   , '기본메뉴'),
+ 		  menu_group_name;
  		 
 -- 메뉴 그룹 이름 순으로 조회 (내림차순)
 -- by changyoun
 SELECT
-		 menu_group_name
+	   menu_group_name
   FROM tb_menu_group
  ORDER BY FIELD(
- 					 menu_group_name
- 				  , '메인메뉴'
-				  , '애피타이저'
-				  , '디저트'
-				  , '기본메뉴');
- 		 menu_group_name DESC;
+ 				 menu_group_name
+ 			   , '메인메뉴'
+			   , '애피타이저'
+			   , '디저트'
+			   , '기본메뉴');
+ 		  menu_group_name DESC;
  		 
  		 
 -- 메뉴 그룹 수정 by changyoun
 UPDATE tb_menu_group
-	SET menu_group_name = '피자는 성민이얼굴피자~'
-	  , menu_group_detail = '얼굴폈니?'
+   SET menu_group_name = '피자는 성민이얼굴피자~'
+	 , menu_group_detail = '얼굴폈니?'
  WHERE restaurant_seq = 1
    AND menu_group_seq = 1;
    
--- waiting_people_sql/restaurant_sql/restaurant_menu_management.sql
+++ waiting_people_sql/restaurant_sql/restaurant_menu_management.sql
@@ -1,19 +1,19 @@
 -- 사진 제외 메뉴 등록 by changyoun
 INSERT
   INTO tb_menu(
-  					menu_group_seq      -- 메뉴 그룹 고유번호
-				 , restaurant_seq      -- 음식점 고유번호
-				 , menu_name           -- 메뉴 이름
-				 , menu_price          -- 메뉴 가격
-				 , menu_ingredient     -- 메뉴 재료
-				 , menu_detail         -- 메뉴 설명
-				 , menu_recommend_yn)  -- 메뉴 추천 여부
+  				menu_group_seq      -- 메뉴 그룹 고유번호
+			  , restaurant_seq      -- 음식점 고유번호
+			  , menu_name           -- 메뉴 이름
+			  , menu_price          -- 메뉴 가격
+			  , menu_ingredient     -- 메뉴 재료
+			  , menu_detail         -- 메뉴 설명
+			  , menu_recommend_yn)  -- 메뉴 추천 여부
 VALUES(
-		 3
+	    3
 	  , 1
 	  , '등록메뉴이름'
 	  , 15000
-	  , '등록한메뉴의 재료1'
+      , '등록한메뉴의 재료1'
 	  , '등록한메뉴설명1'
 	  , 'Y');
 	  
@@ -22,17 +22,17 @@ VALUES(
 
 INSERT
   INTO tb_image_file(
-                     menu_seq               -- 메뉴 고유번호
-                   , picture_name           -- 사진 파일 이름
-                   , picture_original_name  -- 사진 이름
-                   , picture_url            -- 사진 url
-                   , creator)               -- 등록한 사람ID
+                      menu_seq               -- 메뉴 고유번호
+                    , picture_name           -- 사진 파일 이름
+                    , picture_original_name  -- 사진 이름
+                    , picture_url            -- 사진 url
+                    , creator)               -- 등록한 사람ID
 VALUES(
-       1
-     , '사진파일이름'
-     , '사진이름'
-     , '사진url'
-     , 'test8');
+        1
+      , '사진파일이름'
+      , '사진이름'
+      , '사진url'
+      , 'test8');
 
 
 
@@ -40,23 +40,23 @@ VALUES(
 -- 메뉴 그룹 최초 조회
 -- by changyoun
 SELECT
-		 m.menu_name AS '메뉴 이름'
-	  , m.menu_price AS '메뉴 가격'
-	  , mg.menu_group_name AS '메뉴 그룹 이름'
-	  , m.reg_date AS '메뉴 등록일'
-	  , img.picture_name AS '메뉴 사진 파일 이름'
-	  , img.picture_original_name AS '메뉴 사진 이름'
-	  , img.picture_url AS '메뉴 사진 url'
+	   m.menu_name AS '메뉴 이름'
+	 , m.menu_price AS '메뉴 가격'
+	 , mg.menu_group_name AS '메뉴 그룹 이름'
+	 , m.reg_date AS '메뉴 등록일'
+	 , img.picture_name AS '메뉴 사진 파일 이름'
+     , img.picture_original_name AS '메뉴 사진 이름'
+	 , img.picture_url AS '메뉴 사진 url'
   FROM tb_menu m
   LEFT JOIN tb_image_file img ON (m.menu_seq = img.menu_seq)
   LEFT JOIN tb_menu_group mg ON (m.menu_group_seq = mg.menu_group_seq)
  WHERE m.restaurant_seq = 3 
  ORDER BY FIELD(
- 					 mg.menu_group_name
- 				  , '메인메뉴'
-				  , '애피타이저'
-				  , '디저트'
-				  , '기본메뉴');
+ 				 mg.menu_group_name
+ 			   , '메인메뉴'
+			   , '애피타이저'
+			   , '디저트'
+			   , '기본메뉴');
 				  
 
 
@@ -76,91 +76,91 @@ SELECT
   LEFT JOIN tb_menu_group mg ON (m.menu_group_seq = mg.menu_group_seq)
  WHERE m.restaurant_seq = 3 
  ORDER BY FIELD(
- 					 mg.menu_group_name
- 				  , '메인메뉴'
-				  , '애피타이저'
-				  , '디저트'
-				  , '기본메뉴'),
- 			 mg.menu_group_name;
+ 				 mg.menu_group_name
+ 			   , '메인메뉴'
+			   , '애피타이저'
+			   , '디저트'
+			   , '기본메뉴'),
+ 		  mg.menu_group_name;
  
 -- 메뉴 그룹별 조회(그룹별 내림차순)
 -- by changyoun
 SELECT
-		 m.menu_name AS '메뉴 이름'
-	  , m.menu_price AS '메뉴 가격'
-	  , mg.menu_group_name AS '메뉴 그룹 이름'
-	  , m.reg_date AS '메뉴 등록일'
-	  , img.picture_name AS '메뉴 사진 파일 이름'
-	  , img.picture_original_name AS '메뉴 사진 이름'
-	  , img.picture_url AS '메뉴 사진 url'
+	   m.menu_name AS '메뉴 이름'
+	 , m.menu_price AS '메뉴 가격'
+	 , mg.menu_group_name AS '메뉴 그룹 이름'
+	 , m.reg_date AS '메뉴 등록일'
+	 , img.picture_name AS '메뉴 사진 파일 이름'
+	 , img.picture_original_name AS '메뉴 사진 이름'
+	 , img.picture_url AS '메뉴 사진 url'
   FROM tb_menu m
   LEFT JOIN tb_image_file img ON (m.menu_seq = img.menu_seq)
   LEFT JOIN tb_menu_group mg ON (m.menu_group_seq = mg.menu_group_seq)
  WHERE m.restaurant_seq = 3 
  ORDER BY FIELD(
- 					 mg.menu_group_name
- 				  , '메인메뉴'
-				  , '애피타이저'
-				  , '디저트'
-				  , '기본메뉴'),
- 			 mg.menu_group_name DESC;
+ 				 mg.menu_group_name
+ 			   , '메인메뉴'
+			   , '애피타이저'
+			   , '디저트'
+			   , '기본메뉴'),
+ 		  mg.menu_group_name DESC;
 
 -- 메뉴 이름순으로 조회(오름차순)
 -- by changyoun
 SELECT
-		 m.menu_name AS '메뉴 이름'
-	  , m.menu_price AS '메뉴 가격'
-	  , mg.menu_group_name AS '메뉴 그룹 이름'
-	  , m.reg_date AS '메뉴 등록일'
-	  , img.picture_name AS '메뉴 사진 파일 이름'
-	  , img.picture_original_nameas AS '메뉴 사진 이름'
-	  , img.picture_url AS '메뉴 사진 url'
+	   m.menu_name AS '메뉴 이름'
+	 , m.menu_price AS '메뉴 가격'
+	 , mg.menu_group_name AS '메뉴 그룹 이름'
+	 , m.reg_date AS '메뉴 등록일'
+	 , img.picture_name AS '메뉴 사진 파일 이름'
+	 , img.picture_original_nameas AS '메뉴 사진 이름'
+	 , img.picture_url AS '메뉴 사진 url'
   FROM tb_menu m
   LEFT JOIN tb_image_file img ON (m.menu_seq = img.menu_seq)
   LEFT JOIN tb_menu_group mg ON (m.menu_group_seq = mg.menu_group_seq)
  WHERE m.restaurant_seq = 3 
  ORDER BY FIELD(
- 					 mg.menu_group_name
- 				  , '메인메뉴'
-				  , '애피타이저'
-				  , '디저트'
-				  , '기본메뉴'),
-			 m.menu_name, 
-			 mg.menu_group_name;
+ 			     mg.menu_group_name
+ 			   , '메인메뉴'
+			   , '애피타이저'
+			   , '디저트'
+			   , '기본메뉴'),
+		  m.menu_name, 
+		  mg.menu_group_name;
 
 -- 메뉴 이름순으로 조회(내림차순)
 -- by changyoun
 SELECT
-		 m.menu_name AS '메뉴 이름'
-	  , m.menu_price AS '메뉴 가격'
-	  , mg.menu_group_name AS '메뉴 그룹 이름'
-	  , m.reg_date AS '메뉴 등록일'
-	  , img.picture_name AS '메뉴 사진 파일 이름'
-	  , img.picture_original_name AS '메뉴 사진 이름'
-	  , img.picture_url AS '메뉴 사진 url'
+	   m.menu_name AS '메뉴 이름'
+	 , m.menu_price AS '메뉴 가격'
+	 , mg.menu_group_name AS '메뉴 그룹 이름'
+	 , m.reg_date AS '메뉴 등록일'
+	 , img.picture_name AS '메뉴 사진 파일 이름'
+	 , img.picture_original_name AS '메뉴 사진 이름'
+	 , img.picture_url AS '메뉴 사진 url'
   FROM tb_menu m
   LEFT JOIN tb_image_file img ON (m.menu_seq = img.menu_seq)
   LEFT JOIN tb_menu_group mg ON (m.menu_group_seq = mg.menu_group_seq)
  WHERE m.restaurant_seq = 3 
  ORDER BY FIELD(
- 					 mg.menu_group_name
- 				  , '메인메뉴'
-				  , '애피타이저'
-				  , '디저트'
-				  , '기본메뉴'),
- 			 m.menu_name DESC,
-			 mg.menu_group_name;
+ 				 mg.menu_group_name
+ 			   , '메인메뉴'
+			   , '애피타이저'
+			   , '디저트'
+			   , '기본메뉴'),
+ 		  m.menu_name DESC,
+		  mg.menu_group_name;
  
 -- 메뉴 등록일 순으로 조회(오래된 순)
 -- by changyoun
 SELECT
-		 m.menu_name AS '메뉴 이름'
-	  , m.menu_price AS '메뉴 가격'
-	  , mg.menu_group_name AS '메뉴 그룹 이름'
-	  , m.reg_date AS '메뉴 등록일'
-	  , img.picture_name AS '메뉴 사진 파일 이름'
-	  , img.picture_original_name AS '메뉴 사진 이름'
-	  , img.picture_url AS '메뉴 사진 url'
+	   m.menu_name AS '메뉴 이름'
+	 , m.menu_price AS '메뉴 가격'
+	 , mg.menu_group_name AS '메뉴 그룹 이름'
+	 , m.reg_date AS '메뉴 등록일'
+	 , img.picture_name AS '메뉴 사진 파일 이름'
+	 , img.picture_original_name AS '메뉴 사진 이름'
+     , img.picture_url AS '메뉴 사진 url'
   FROM tb_menu m
   LEFT JOIN tb_image_file img ON (m.menu_seq = img.menu_seq)
   LEFT JOIN tb_menu_group mg ON (m.menu_group_seq = mg.menu_group_seq)
@@ -170,13 +170,13 @@ SELECT
 -- 메뉴 등록일 순으로 조회(최신 순)
 -- by changyoun
 SELECT
-		 m.menu_name AS '메뉴 이름'
-	  , m.menu_price AS '메뉴 가격'
-	  , mg.menu_group_name AS '메뉴 그룹 이름'
-	  , m.reg_date AS '메뉴 등록일'
-	  , img.picture_name AS '메뉴 사진 파일 이름'
-	  , img.picture_original_name AS '메뉴 사진 이름'
-	  , img.picture_url AS '메뉴 사진 url'
+	   m.menu_name AS '메뉴 이름'
+	 , m.menu_price AS '메뉴 가격'
+	 , mg.menu_group_name AS '메뉴 그룹 이름'
+	 , m.reg_date AS '메뉴 등록일'
+	 , img.picture_name AS '메뉴 사진 파일 이름'
+	 , img.picture_original_name AS '메뉴 사진 이름'
+	 , img.picture_url AS '메뉴 사진 url'
   FROM tb_menu m
   LEFT JOIN tb_image_file img ON (m.menu_seq = img.menu_seq)
   LEFT JOIN tb_menu_group mg ON (m.menu_group_seq = mg.menu_group_seq)
@@ -188,7 +188,7 @@ SELECT
 
 --	음식점 메뉴 상세 조회 by changyoun
 SELECT 
-		 m.menu_name AS '메뉴 이름'
+	   m.menu_name AS '메뉴 이름'
      , m.menu_price AS '메뉴 가격'
      , m.menu_ingredient AS '메뉴 재료'
      , m.menu_detail AS '메뉴 설명'
@@ -206,25 +206,25 @@ SELECT
 -- 음식점 메뉴 수정 (사진 수정 제외) 
 -- by changyoun
 UPDATE
-		 tb_menu
+	   tb_menu
    SET menu_name = '수정 메뉴 이름'
- 	  , menu_price = 3000
-	  , menu_ingredient = '수정된 메뉴의 재료'
-	  , menu_detail = '수정된 메뉴의 설명'
-	  , menu_recommend_yn = 'N'
-	  , reg_date = CURRENT_TIMESTAMP	
+ 	 , menu_price = 3000
+	 , menu_ingredient = '수정된 메뉴의 재료'
+	 , menu_detail = '수정된 메뉴의 설명'
+	 , menu_recommend_yn = 'N'
+	 , reg_date = CURRENT_TIMESTAMP	
  WHERE menu_seq = 1;
 
 -- 음식점 메뉴 수정 (사진 수정 포함) 
 -- by changyoun
 UPDATE
-		 tb_menu
+	   tb_menu
    SET menu_name = '수정 메뉴 이름'
- 	  , menu_price = 3000
-	  , menu_ingredient = '수정된 메뉴의 재료'
-	  , menu_detail = '수정된 메뉴의 설명'
-	  , menu_recommend_yn = 'N'
-	  , reg_date = CURRENT_TIMESTAMP	
+ 	 , menu_price = 3000
+	 , menu_ingredient = '수정된 메뉴의 재료'
+	 , menu_detail = '수정된 메뉴의 설명'
+	 , menu_recommend_yn = 'N'
+	 , reg_date = CURRENT_TIMESTAMP	
  WHERE menu_seq = 1;
 
 UPDATE tb_image_file
